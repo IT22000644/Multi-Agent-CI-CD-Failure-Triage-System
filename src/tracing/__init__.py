b0256113efbc9db7738605ea6b76675fbbf59250
IT22000644 <manilka.projects@gmail.com>
2026-04-30 03:53:39 +0530

    Add JSONL trace logger and tests

--- src/tracing/__init__.py
+++ src/tracing/__init__.py
@@ -3,3 +3,7 @@
 This module will provide utilities for capturing, storing, and querying
 trace events during workflow execution.
 """
+
+from src.tracing.trace_logger import write_trace_event, write_trace_events
+
+__all__ = ["write_trace_event", "write_trace_events"]
